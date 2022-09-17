2725f948915da4b2d4e64c587b52e1cf1d4aa554
FROST066 <ppdollar0@gmail.com>
2022-09-17 18:08:48 +0100

    finish Screen test

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -12,6 +12,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:speech_to_text/speech_to_text_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -23,5 +24,6 @@ void registerPlugins(Registrar registrar) {
   FirebaseDatabaseWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
+  SpeechToTextPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
